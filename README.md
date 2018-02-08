9399a789f7a3ecc5857ed5dcdc41affee931cb3b
Philipp Stehle <philipp.stehle@sap.com>
2018-02-08 16:34:05 +0100

    POC

---
+++ README.md
@@ -0,0 +1,26 @@
+# Nadire - the Random Data Server (WIP)
+**Disclaimer**
+This is one of my first times using Go.
+Don't expect good (or even average) code quality and feel free to open bug on bad code smell.
+
+### Build
+1. Download and install Go.
+2. Clone repo
+3. Navigate to repo with your terminal
+4. Run `go build`. This will build Nadire for your operating system and processor architecture.
+
+### Usage
+Start the executable, send as many request as you want.
+
+Port:
+- the first argument will be used as port
+- default port is 4321
+
+Examples:
+- [?size=1k](http://localhost:4321/?size=1k) returns 1 KB of random data.
+- [?delay=10000&size=1k](http://localhost:4321/?delay=10000&size=1k) returns 1 KB of random data after a delay of (about) 10 seconds.
+- [?size=100M](http://localhost:4321/?size=100M) returns 100 MB of random data.
+- [?size=512](http://localhost:4321/?size=512) returns 512 Byte of random data.
+
+### Name
+As the Server generated random data it's name was chosen by random via [behindthename.com](https://www.behindthename.com/random/random.php?number=1&gender=both&surname=&all=yes)
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+	"math/rand"
+	"os"
+)
+
+
+const BUFFERSIZE = 1024 * 1024
+
+const randomStringContent = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.:"; // length of 64
+func getRandomString(buffer []byte){
+	for i := range buffer {
+        buffer[i] = randomStringContent[rand.Int() & 63]
+    }
+}
+
+func handler(rw http.ResponseWriter, r *http.Request) {
+	strDelay := r.URL.Query().Get("delay")
+	strSize  := r.URL.Query().Get("size")
+	
+	sizeInByte := 42
+	if(len(strSize) > 0) {
+		faktor := 1
+		if(len(strSize) > 1) {
+			if(strSize[len(strSize)-1] == 'k') { // Kilobyte
+				faktor = 1024
+				strSize = strSize[:len(strSize)-1]
+			} else if(strSize[len(strSize)-1] == 'M') { // Megabyte
+				faktor = 1024 * 1024
+				strSize = strSize[:len(strSize)-1]
+			}
+		}
+		size, err := strconv.Atoi(strSize)
+		if (err != nil) {
+			fmt.Println(err)
+			size = 1
+			faktor = 1
+		}
+		sizeInByte = faktor * size
+	}
+
+	delay, err := strconv.Atoi(strDelay)
+	if (err != nil) {
+		delay = 0
+		if (len(strDelay) > 0) {
+			fmt.Println(err)
+		}
+	}
+	
+	
+	fmt.Println("Got request with delay: " + strconv.Itoa(delay) + " and size: " + strconv.Itoa(sizeInByte))
+	
+	time.Sleep(time.Duration(delay) * time.Millisecond)
+	
+	rw.Header()["Content-Type"] = []string{"text/plain"}
+	rw.Header()["Content-Length"] = []string{strconv.Itoa(sizeInByte)}
+	
+	buffer := make([]byte, BUFFERSIZE)
+	restSizeInByte := sizeInByte
+	for (restSizeInByte > 0) {
+		if(restSizeInByte < BUFFERSIZE) {
+			buffer = buffer[:restSizeInByte]
+		}
+		getRandomString(buffer)
+		restSizeInByte -= len(buffer)
+		rw.Write(buffer)
+	}
+}
+
+func main() {
+
+	if(len(os.Args) > 2) {
+		fmt.Println("Nadire expects zero or one arguments")
+		os.Exit(1)
+	}
+	
+	port := "4321"
+	if(len(os.Args) == 2) {
+		port = os.Args[1]
+	}
+
+	http.HandleFunc("/", handler)
+
+	fmt.Println("starting Nadire on " + port)
+	fmt.Println(http.ListenAndServe(":" + port, nil))
+}
